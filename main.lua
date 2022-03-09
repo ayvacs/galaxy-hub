1c2316c979f5d053e630b3c1832086eac0e48954
dom <dompec07@gmail.com>
2022-03-09 12:14:21 -0500

    Test

---
+++ main.lua
@@ -0,0 +1,22 @@
+return {
+    ["PluginName"] = "Galaxy Hub",
+    ["PluginDescription"] = "A cool scripthub",
+    ["Commands"] = {
+        ["cmd-x"] = {
+            ["ListName"] = "CMD-X",
+            ["Description"] = "Launches CMD-X",
+            ["Aliases"] = {"-domain-x", "cmdx", "cx"},
+            ["Function"] = function(args, speaker)
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source", true))()
+            end
+        },
+        ["domain-x"] = {
+            ["ListName"] = "Domain X",
+            ["Description"] = "Launches Domain X",
+            ["Aliases"] = {"-domain-x", "dmnx", "dx"},
+            ["Function"] = function(args, speaker)
+                loadstring(game:HttpGet("https://shlex.dev/release/domainx/latest.lua", true))()
+            end
+        }
+    }
+}
