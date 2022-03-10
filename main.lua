79e1df94708f715f3ccef5c41629c7b080b8a6b3
dom <dompec07@gmail.com>
2022-03-09 22:40:44 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,72 +1,107 @@
---return loadstring(game:HttpGet("https://ave.is-a.dev/galaxy-hub", true))()
-
 -- https://ave.is-a.dev/galaxy-hub
-ver=0
+-- v1.0.0
+
+
+meta = {
+    _name = "Galaxy Hub",
+    _desc = "Launch other scripts and script hubs easily, through Infinite Yield.",
+    _author = "ayvacs @ GitHub",
+
+    loadsig = "[ Loaded with Galaxy Hub ]\n\n"
+}
+
+print(meta._name)
 
 
 function ls(url)
+    -- Loadstring function; loads url.
     return loadstring(game:HttpGet(url, true))()
 end
 
 function desc(a, notes)
-    print(a)
+    --[[
+        a = authors table [OPTIONAL]
+            i.e. {"ScriptPublisher", "Author1", "Author2"}
+        notes = script notes [OPTIONAL]
+            i.e. "Currently having compatibility issues with Mac injectors."
+    ]]
     if a then
         local authors = ""
-        for i, v in ipairs(a) do
-            if i == #a then
-                authors = authors .. ", and " .. v
-            else
-                authors = authors .. ", " .. v
+        
+        if #a == 1 then
+            -- When there's only one author
+            authors = a[1]
+        elseif #a == 2 then
+            -- When there's two authors
+            authors = a[1] .. " and " .. a[2]
+        else
+            -- When there's 3+ authors
+            for i, v in pairs(a) do
+                if i == 1 then
+                    authors = v
+                else
+                    if i == #a then
+                        authors = authors .. ", and " .. v
+                    else
+                        authors = authors .. ", " .. v
+                    end
+                end
             end
         end
 
         if notes then
-            return "Written by " .. authors .. ". " .. notes .. " [ Loaded with Galaxy Hub ]"
+            return meta.loadsig .. "Written by " .. authors .. ". " .. notes
         else
-            return "Written by " .. authors .. ". [ Loaded with Galaxy Hub ]"
+            return meta.loadsig .. "Written by " .. authors .. ". "
         end
     else
         if notes then
-            return notes .. " [ Loaded with Galaxy Hub ]"
+            return meta.loadsig .. "Written by an unknown author." .. notes
         else
-            return "[ Loaded with Galaxy Hub ]"
+            return meta.loadsig .. "Written by an unknown author."
         end
     end
 end
 
 
 return {
-    ["PluginName"] = "Galaxy Hub",
-    ["PluginDescription"] = "A cool scripthub",
+    ["PluginName"] = meta._name,
+    ["PluginDescription"] = meta._desc,
     ["Commands"] = {
+        -- Galaxy Maintenance Scripts
+
         ["galaxy"] = {
-            ["ListName"] = "galaxy help",
+            ["ListName"] = " galaxy help",
             ["Description"] = "Welcome to GalaxyHub!",
             ["Aliases"] = {"gh", "ghub", "ghelp"},
             ["Function"] = function(args, speaker)
                 print("Loaded the help script!!")
             end
         },
+
+
+        -- Community Scripts
+
         ["cmd-x"] = {
-            ["ListName"] = "CMD-X",
+            ["ListName"] = "cmd-x",
             ["Description"] = desc({"xxherts", "Curvn"}),
-            ["Aliases"] = {"-domain-x", "cmdx", "cx"},
+            ["Aliases"] = {"cmdx", "cx"},
             ["Function"] = function(args, speaker)
                 ls("https://raw.githubusercontent.com/CMD-X/CMD-X/master/Source")
             end
         },
         ["domain-x"] = {
-            ["ListName"] = "Domain X",
+            ["ListName"] = "domain-x",
             ["Description"] = desc({"shlex", "Sirius Software"}),
-            ["Aliases"] = {"-domain-x", "dmnx", "dx"},
+            ["Aliases"] = {"domainx", "dmnx", "dx"},
             ["Function"] = function(args, speaker)
                 ls("https://shlex.dev/release/domainx/latest.lua")
             end
         },
         ["solaris"] = {
-            ["ListName"] = "Solaris Hub",
+            ["ListName"] = "solaris",
             ["Description"] = desc({"trill"}),
-            ["Aliases"] = {"sh"},
+            ["Aliases"] = {"solaris-hub", "sh"},
             ["Function"] = function(args, speaker)
                 ls("https://solarishub.dev/script.lua")
             end
