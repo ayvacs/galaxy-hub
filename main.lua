242d87742edd53b1a1952e7c8959c2836c74e9d3
ave <ayvacs@proton.me>
2022-08-29 01:30:14 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -83,7 +83,7 @@ return {
             ["Description"] = desc({"Moon", "Courtney"}, "Made for Synapse"),
             ["Aliases"] = {"dark-dex", "darkdex", "dd", "d-dex", "ddex"},
             ["Function"] = function(args, speaker)
-                ls("https://ave.is-a.dev/scripts/dark-dex")
+                ls("https://gist.githubusercontent.com/DinosaurXxX/b757fe011e7e600c0873f967fe427dc2/raw/ee5324771f017073fc30e640323ac2a9b3bfc550/dark%2520dex%2520v4")
             end
         },
         ["domain-x"] = {
